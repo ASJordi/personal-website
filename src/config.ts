71edd9839074a8adee9a27ffa65a481401f4ddf7
Jordi Ayala <jord7as@gmail.com>
2023-01-28 15:47:01 -0600

    feat(cv): :sparkles: updated intro

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Jordi Eduardo Ayala Soto | Portfolio';
-export const SITE_DESCRIPTION = 'Jordi Eduardo Ayala Soto | Portfolio';
+export const SITE_TITLE = 'Jordi Eduardo Ayala Soto';
+export const SITE_DESCRIPTION = 'Hola, soy Ingeniero en Sistemas Computaciones recién egresado y actualmente me encuentro en la búsqueda de trabajo como desarrollador Fullstack. Tengo experiencia Académica/Laboral en proyectos web y de escritorio con tecnologías como Java, Python y JavaScript, así como HTML, CSS, Tailwind CSS y React. Además de manejo de base de datos MySQL, SQL Server y MongoDB. Me gusta estar en constante aprendizaje y poder aplicarlo en el mundo tecnológico.';
