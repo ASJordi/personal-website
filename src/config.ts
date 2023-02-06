bde550611cda2dc1ae4c54f2af69483d095b3766
Jordi Ayala <jord7as@gmail.com>
2023-02-06 17:39:14 -0600

    fix: :bug: update description

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Jordi Eduardo Ayala Soto';
-export const SITE_DESCRIPTION = 'Ingeniero en Sistemas Computacionales recién egresado y actualmente me encuentro en la búsqueda de trabajo como desarrollador Fullstack. Tengo experiencia Académica/Laboral en proyectos web y de escritorio con tecnologías como Java, Python y JavaScript, así como HTML, CSS, Tailwind CSS y React. Además de manejo de base de datos MySQL, SQL Server y MongoDB. Me gusta estar en constante aprendizaje y poder aplicarlo en el mundo tecnológico.';
+export const SITE_DESCRIPTION = 'IIngeniero en Sistemas Computacionales recién egresado y actualmente en búsqueda de trabajo como desarrollador FullStack. Tengo experiencia Académica/Laboral en proyectos web y de escritorio con tecnologías como Java, Python y JavaScript, así como, HTML, CSS, Tailwind CSS y React. Además de manejo de base de datos MySQL, SQL Server y MongoDB. Me apasiona la tecnología y estar en constante aprendizaje.';
