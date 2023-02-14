91755040bea6adaa7becd61ae1c3b4c31845712d
Jordi Ayala <jord7as@gmail.com>
2023-02-14 10:19:51 -0600

    chore: update rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,19 @@
 import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-export const get = () =>
-	rss({
+export async function get() {
+	const blog = await getCollection('blog');
+	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.md'),
+		items: blog.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
+			customData: post.data.customData,
+			link: `/blog/${post.slug}/`,
+		})),
 	});
+}
