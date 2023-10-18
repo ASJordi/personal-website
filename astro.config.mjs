0dfc66f7181257e83f2c7609b0c748186f7f9b3c
Jordi Ayala <jord7as@gmail.com>
2023-10-18 13:04:21 -0600

    build: :arrow_up: upgrade and remove dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import image from "@astrojs/image";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://asjordi.dev',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )]
+  integrations: [mdx(), sitemap(), tailwind()]
 });
