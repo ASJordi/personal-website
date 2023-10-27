b442189f54f4964896866c5cc46d3ef8037413f2
Jordi Ayala <jord7as@gmail.com>
2023-10-26 19:25:12 -0600

    chore: :mag: update tags

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,4 @@
 
 export const SITE_TITLE = 'Jordi Eduardo Ayala Soto';
 export const SITE_DESCRIPTION = 'Ingeniero en Sistemas Computacionales recién egresado y actualmente en búsqueda de trabajo como desarrollador FullStack. Tengo experiencia Académica/Laboral en proyectos web y de escritorio con tecnologías como Java, Python y JavaScript, así como, HTML, CSS, Tailwind CSS y React. Además de manejo de base de datos MySQL, SQL Server y MongoDB. Me apasiona la tecnología y estar en constante aprendizaje.';
+export const SITE_AUTHOR = '@ASJordi';
