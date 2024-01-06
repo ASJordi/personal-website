49220ef8220565f0067572d836c8b45bd8854ecf
Jordi Ayala <jord7as@gmail.com>
2024-01-06 11:49:25 -0600

    refactor: :recycle: add tags to rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,19 +1,27 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
 
-export async function get() {
+const parser = new MarkdownIt();
+
+export async function GET() {
 	const blog = await getCollection('blog');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
-		items: blog.map((post) => ({
-			title: post.data.title,
-			pubDate: post.data.pubDate,
-			description: post.data.description,
-			customData: post.data.customData,
-			link: `/blog/${post.slug}/`,
-		})),
+		items: blog.map((post) => {
+			return {
+				title: post.data.title,
+				pubDate: post.data.pubDate,
+				description: post.data.description,
+				link: `/blog/${post.slug}/`,
+				categories: post.data.tags,
+				content: sanitizeHtml(parser.render(post.body)),
+				...post.data,
+			};
+		}),
 	});
 }
